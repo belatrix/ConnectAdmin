ed64ea55f4ed16e595556bdce6e597acf3a696de
John Machahuay <jmachahuay@belatrixsf.com>
2017-03-21 11:46:54 -0500

    Refactor Actions creators. Added functionality for redux-promise-middleware

--- src/js/reducers/employeeReducer.js
+++ src/js/reducers/employeeReducer.js
@@ -12,7 +12,7 @@ export default function reducer(state = {
       return { ...state, fetching: false, error: action.payload };
 
     case 'FETCH_LIST_EMPLOYEES_FULFILLED':
-      return { ...state, fetching: false, fetched: true, list: action.payload.results };
+      return { ...state, fetching: false, fetched: true, list: action.payload.data.results };
     default:
       return state;
   }
--- src/js/reducers/scoreReducer.js
+++ src/js/reducers/scoreReducer.js
@@ -8,28 +8,28 @@ export default function reducer(state = {
       return { ...state, fetching: true };
 
     case 'FETCH_LAST_REJECTED':
-      return { ...state, fetching: false, error: action.payload };
+      return { ...state, fetching: false, error: action.payload.data };
 
     case 'FETCH_LAST_FULFILLED':
-      return { ...state, fetching: false, fetched: true, lastMonth: action.payload };
+      return { ...state, fetching: false, fetched: true, lastMonth: action.payload.data };
 
     case 'FETCH_CURRENT_PENDING':
       return { ...state, fetching: true };
 
     case 'FETCH_CURRENT_REJECTED':
-      return { ...state, fetching: false, error: action.payload };
+      return { ...state, fetching: false, error: action.payload.data };
 
     case 'FETCH_CURRENT_FULFILLED':
-      return { ...state, fetching: false, fetched: true, currentMonth: action.payload };
+      return { ...state, fetching: false, fetched: true, currentMonth: action.payload.data };
 
     case 'FETCH_TOTAL_PENDING':
       return { ...state, fetching: true };
 
     case 'FETCH_TOTAL_REJECTED':
-      return { ...state, fetching: false, error: action.payload };
+      return { ...state, fetching: false, error: action.payload.data };
 
     case 'FETCH_TOTAL_FULFILLED':
-      return { ...state, fetching: false, fetched: true, total: action.payload };
+      return { ...state, fetching: false, fetched: true, total: action.payload.data };
     default:
       return state;
   }
